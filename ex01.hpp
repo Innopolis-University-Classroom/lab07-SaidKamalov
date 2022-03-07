3345c7ac6100e1b1ead4383a3a1074e089c55c7b
SaidKamalov <arturrulit5@gmail.com>
2022-03-07 16:50:20 +0300

    Said Kamalov

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,11 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
+template<typename T>
 void multiples (int& sum, int x, int n)
 {
-
+    sum=1;
+    for(int i = 0;i<n;++i){
+        sum+=x*i;
+    }
 }
